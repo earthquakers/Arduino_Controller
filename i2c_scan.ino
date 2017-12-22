04ea377e0344bec8fdc7824f1a183640bb1c840c
Earthquakers FTC <earthquakers.ftc@gmail.com>
2017-12-22 20:51:28 +0100

    dingen

---
+++ i2c_scan.ino
@@ -0,0 +1,45 @@
+/**
+ * Het address is 8
+ */
+ 
+#include <Wire.h>
+#include <stdint.h>
+
+void scan() {
+  char buff[8];
+
+  for(uint8_t addr = 1; addr <= 128; addr++) {
+    uint8_t ret = Wire.requestFrom(addr, 8);
+
+    uint8_t pos = 0;
+    
+    while(Wire.available()) {
+      buff[pos++] = Wire.read();
+    }
+
+    Serial.print("addr: ");
+    Serial.print(addr);
+    Serial.print(", ret: ");
+    Serial.print(ret);
+    Serial.print(", resp: '");
+    
+    for(uint8_t i = 0; i < 8; i++) {
+      Serial.print(buff[i]);
+    }
+    
+    Serial.println("'");
+  }
+
+  delay(10);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();  
+}
+
+void loop() {
+  scan();
+
+  delay(1000);
+}
---
+++ sketch_dec22a.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <stdint.h>
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();  
+}
+
+void loop() {
+  uint8_t buff[0x75];
+
+  memset(buff, 0, 0x75);
+
+  buff[0x44] = 1;
+  buff[0x56] = 128;
+  buff[0x57] = 0b0000000;
+
+  Wire.beginTransmission(8);
+  Wire.write(buff, 0x75);
+  Wire.endTransmission();
+  
+  delay(100);
+}
